509a28dc8c621f5b0e6bef972c837dc9b3b92ebc
Vigi <vigneshnataraj@gmail.com>
2017-11-27 08:19:38 +1100

    implement jwt

---
+++ API/routes/auth.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const authMiddleware = require('../middleware/auth')
+
+const router = express.Router()
+
+// Sign in: POST /auth
+router.post('/auth/login', authMiddleware.authenticateSignIn, (req, res) => {
+  res.json({success: true})
+})
+
+// Registration: POST /auth/register
+router.post('/auth/register', authMiddleware.register, (req, res) => {
+  res.json({ user: req.user })
+})
+
+
+
+module.exports = router
