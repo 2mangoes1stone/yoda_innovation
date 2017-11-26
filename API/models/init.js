956bc50a1728256e3ed7bb897bdc3029674a09a4
Vigi <vigneshnataraj@gmail.com>
2017-11-27 10:58:56 +1100

    use local db

--- API/models/init.js
+++ API/models/init.js
@@ -1,17 +1,7 @@
 // Initializing mongoose here so dont have to continually write
 const mongoose = require('mongoose')
 
-// mongoose.connection.on("open", function(ref) {
-//   console.log("Connected to mongo server.");
-//   return start_up();
-// });
-
-// mongoose.connection.on("error", function(err) {
-//   console.log("Could not connect to mongo server!");
-//   return console.log(err);
-// });
-
-mongoose.connect(process.env.DATABASE, { useMongoClient: true })
+mongoose.connect('mongodb://localhost/test', { useMongoClient: true })
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
